69069e5c137312b73e5f1de639d297828ac69832
Mariana Silva <mariana.silva.a46@hotmail.com>
2025-03-11 09:11:52 -0300

    alterações no fd e componentes

--- arquivos/comparador_85.v
+++ arquivos/comparador_85.v
@@ -15,15 +15,15 @@
 
 module comparador_85 (ALBi, AGBi, AEBi, A, B, ALBo, AGBo, AEBo);
 
-    input[3:0] A, B;
+    input[15:0] A, B;
     input      ALBi, AGBi, AEBi;
     output     ALBo, AGBo, AEBo;
-    wire[4:0]  CSL, CSG;
+    wire[16:0]  CSL, CSG;
 
     assign CSL  = ~A + B + ALBi;
-    assign ALBo = ~CSL[4];
+    assign ALBo = ~CSL[16];
     assign CSG  = A + ~B + AGBi;
-    assign AGBo = ~CSG[4];
+    assign AGBo = ~CSG[16];
     assign AEBo = ((A == B) && AEBi);
 
 endmodule /* comparador_85 */
